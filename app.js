4ea8b1aabc0cff9ef07a4c86b6404a3071d69699
Ali <alifeyza4017@gmail.com>
2022-09-26 23:37:39 +0300

    added

--- app.js
+++ app.js
@@ -2,16 +2,47 @@ const input = document.querySelector("#input")
 const btn = document.querySelector("#btn")
 const content = document.querySelector(".content")
 const container = document.querySelector(".container")
+let isOkey =false;
+// btn.addEventListener("click", function(){
 
-btn.addEventListener("click", function(){
+//     if(!input.value) {
+//         const p = document.createElement("p")
+//         p.setAttribute("class", "invalid")
+//         container.appendChild(p)
+//         p.innerText = `Please enter a valid country name`
+//     }
+//     input.value = ""
 
-    if(!input.value) {
-        const p = document.createElement("p")
-        p.setAttribute("class", "invalid")
-        container.appendChild(p)
-        p.innerText = `Please enter a valid country name`
+
+// })
+
+const fetchByName = ()=> {
+
+    const url = "https://restcountries.com/v3.1/all"
+
+    fetch(url).then(function(res) {
+        if(!res.ok){
+        isOkey=true;
+        throw new Error(`something went wrong ${res.status}`)
+        }
+        return res.json()
+    }).then((data)=> render(data)).catch((err)=> console.log(err))
+}
+
+fetchByName();
+
+
+const render = (countryName)=> {
+    clg
+
+const content = document.querySelector(".content");
+        console.log(countryName)
+    if(isOkey) {
+        container.style.display = "none"
+        content.innerHTML = `
+        <h2>Something wrong</h2>
+        <img src="./img/404.png">
+        `
     }
-    input.value = ""
+}
 
-
-})
