fd9e929aaecc21deed5a08acaee01fa79d0c994e
Ali <alifeyza4017@gmail.com>
2022-09-26 23:41:50 +0300

    added

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ const btn = document.querySelector("#btn")
 const content = document.querySelector(".content")
 const container = document.querySelector(".container")
 let isOkey =false;
-// btn.addEventListener("click", function(){
+btn.addEventListener("click", function(){
 
-//     if(!input.value) {
-//         const p = document.createElement("p")
-//         p.setAttribute("class", "invalid")
-//         container.appendChild(p)
-//         p.innerText = `Please enter a valid country name`
-//     }
-//     input.value = ""
+    if(!input.value) {
+        const p = document.createElement("p")
+        p.setAttribute("class", "invalid")
+        container.appendChild(p)
+        p.innerText = `Please enter a valid country name`
+    }
+    input.value = ""
 
 
-// })
+})
 
 const fetchByName = ()=> {
 
-    const url = "https://restcountries.com/v3.1/all"
+    const url = "https://estcountries.com/v3.1/all"
 
     fetch(url).then(function(res) {
         if(!res.ok){
         isOkey=true;
-        throw new Error(`something went wrong ${res.status}`)
+        // throw new Error(`something went wrong ${res.status}`)
         }
         return res.json()
     }).then((data)=> render(data)).catch((err)=> console.log(err))
@@ -32,17 +32,5 @@ const fetchByName = ()=> {
 fetchByName();
 
 
-const render = (countryName)=> {
-    clg
 
-const content = document.querySelector(".content");
-        console.log(countryName)
-    if(isOkey) {
-        container.style.display = "none"
-        content.innerHTML = `
-        <h2>Something wrong</h2>
-        <img src="./img/404.png">
-        `
-    }
-}
 
