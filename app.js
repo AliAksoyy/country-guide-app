ab49e8e1359d16fc7f2f2b5e81fd5731ddabb891
Ali <alifeyza4017@gmail.com>
2022-09-27 07:30:08 +0300

    added

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const content = document.querySelector(".content")
 const container = document.querySelector(".container")
 
 
-btn.addEventListener("click", function() {
-  fetchByCountry()
-})
 
 
-const  fetchByCountry = ()=> {
 
-  const url = `https://restcountries.com/v3.1/all/name/turkey}`;
+const  fetchByCountry = (name)=> {
+
+  const url = `https://restcountries.com/v3.1/name/${name}`;
 
   fetch(url)
     .then((res) => {
@@ -37,14 +35,39 @@ const  fetchByCountry = ()=> {
       continents,
       population,
       currencies,
-      languages: { spa },
+      languages: { tur },
     } = countries[0];
 
+      console.log(svg,common,capital,continents,population,Object.values(currencies)[0].name,tur)
+      content.innerHTML = `
+      <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+       <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       </div>
+       <ul class="list-group list-group-flush">
+       <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+       </ul>
+       <div class="card-body">
+         <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+        </div>
+        </div>
+      
+      
+      
+      
+      `;
     
   }
  
 
 
+
+  fetchByCountry("turkey");
 
 
 
