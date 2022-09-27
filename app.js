12f4b8b5c702acf31d0ecebed66de79474377900
Ali <alifeyza4017@gmail.com>
2022-09-27 06:44:06 +0300

    added

--- app.js
+++ app.js
@@ -2,37 +2,33 @@ const input = document.querySelector("#input")
 const btn = document.querySelector("#btn")
 const content = document.querySelector(".content")
 const container = document.querySelector(".container")
-let isOkey =false;
-btn.addEventListener("click", function(){
-
-    if(!input.value) {
-        const p = document.createElement("p")
-        p.setAttribute("class", "invalid")
-        container.appendChild(p)
-        p.innerText = `Please enter a valid country name`
-    }
-    input.value = ""
 
 
+btn.addEventListener("click", function() {
+  fetchByCountry()
 })
 
-const fetchByName = ()=> {
 
-    const url = "https://restcountries.com/v3.1/all"
+const  fetchByCountry = ()=> {
 
-    fetch(url).then(function(res) {
-        if(!res.ok){
-        isOkey=true;
-        throw new Error(`something went wrong ${res.status}`)
-        }
-        return res.json()
-    }).then((data)=> render(data)).catch((err)=> console.log(err))
+  const url = `https://restcountries.com/v3.1/all/name/turkey}`;
+
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`Something went wrong ${res.status}`);
+      }
+      return res.json();
+    })
+    .then(function (data) {
+      renderData(data);
+    })
+    .catch((err) => console.log(err));
 }
 
-fetchByName();
 
-const render = (countryName) => {
-    console.log(countryName)
+  const renderData = (countries)=> {
+    console.log(countries)
 
     const {
       flags: { svg },
@@ -42,29 +38,80 @@ const render = (countryName) => {
       population,
       currencies,
       languages: { spa },
-    } = countryName[0];
-    console.log(svg,common,capital[0],continents[0],currencies.GTQ["name"],spa)
-    const a = createElement("div")
-  content.appendChild(a)
-    a.innerHTML = `
-                <div class="card" style="width: 18rem;">
-  <img src="${svg}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
-    `;
-}
+    } = countries[0];
+
+    
+  }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let isOkey =false;
+// btn.addEventListener("click", function(){
+
+//     if(!input.value) {
+//         const p = document.createElement("p")
+//         p.setAttribute("class", "invalid")
+//         container.appendChild(p)
+//         p.innerText = `Please enter a valid country name`
+//     }
+//     input.value = ""
+
+
+// })
+
+
+
+
+
+//     
+//     console.log(svg,common,capital[0],continents[0],currencies.GTQ["name"],spa)
+//     const a = createElement("div")
+//   content.appendChild(a)
+//     a.innerHTML = `
+//                 <div class="card" style="width: 18rem;">
+//   <img src="${svg}" class="card-img-top" alt="...">
+//   <div class="card-body">
+//     <h5 class="card-title">Card title</h5>
+//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//   </div>
+//   <ul class="list-group list-group-flush">
+//     <li class="list-group-item">An item</li>
+//     <li class="list-group-item">A second item</li>
+//     <li class="list-group-item">A third item</li>
+//   </ul>
+//   <div class="card-body">
+//     <a href="#" class="card-link">Card link</a>
+//     <a href="#" class="card-link">Another link</a>
+//   </div>
+// </div>
+//     `;
+// }
 
 
 
